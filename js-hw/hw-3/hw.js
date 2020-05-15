1cbc78ae28ad3a06d31dec923e32c3c6af78dc51
oddfrid <51491129+oddfrid@users.noreply.github.com>
2020-05-15 16:59:06 +0300

    variables fixed

--- js-hw/hw-3/hw.js
+++ js-hw/hw-3/hw.js
@@ -6,14 +6,14 @@
     следуя рекомендациям по именованию переменных
 */
 
-var age = 20;
-var text = `Ваш возраст: ${age} лет`;
+const age = 20;
+const text = `Ваш возраст: ${age} лет`;
 alert(text); // Должен вывести: Ваш возраст: 20 лет
 
 
-var hours = 12;
-var minutes = 45;
+let hours = 12;
+const minutes = 45;
 ++hours;
-var textWithTime = `Текущее время: ${hours}:${minutes}`;
+const textWithTime = `Текущее время: ${hours}:${minutes}`;
 
 alert(textWithTime); // Должен вывести: Текущее время: 13:45
